4e9cdd8de6625a839555e1f938616df3922a252f
AnabelCristina <allonsyanabel@gmail.com>
2022-03-19 13:05:18 -0300

    Alteraçao tela inicio

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,22 +2,22 @@
   <div class="home">
     <h2 class="text-h5 text-center mb-3 mt-5">Ação contra as fake news</h2>
     <v-img
-      class="home-image mb-4" alt="real ou feiki" src="https://imageproxy.ifunny.co/crop:x-20,resize:640x,quality:90x75/images/49825c86590cb82a99b9881362515e2eee43423949f93fd94643b2da00dcc6ac_1.jpg">
+      class="home-image mb-4 mr-4 ml-4" alt="real ou feiki" src="https://imageproxy.ifunny.co/crop:x-20,resize:640x,quality:90x75/images/49825c86590cb82a99b9881362515e2eee43423949f93fd94643b2da00dcc6ac_1.jpg">
     ></v-img>
 
-    <p>
+    <p class='mb-4 mr-4 ml-4'>
       Tendo em vista o atual cenário onde a tecnologia se tornou uma
       potencial ameaça, nos deparamos diariamante com as famigeradas Fake News.
       A proposta desse site é mitigar o dano causado por elas, trazendo fontes
       confiáveis para se informar. Abaixo pode-se conferir as últimas notícias virais
       e sua veracidade.
     </p>
+
+    
   </div>
 </template>
 
 <script>
-
-
 
 export default {
   name: 'HomeView',
