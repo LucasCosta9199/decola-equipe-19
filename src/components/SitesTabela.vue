ee2a7027ac42672557b842f34ecf5d8567d44d69
Lucas Costa <lucascosta9199@gmail.com>
2022-03-19 16:54:18 -0300

    resolvendo links

--- src/components/SitesTabela.vue
+++ src/components/SitesTabela.vue
@@ -3,15 +3,22 @@
     <template v-slot:default>
       <tbody>
         <tr class="linha-tabela" v-for="site of sitesLista" :key="site.id">
-            <div>
-                <img class="center home-image mt-4 mb-4 mr-4 ml-4"  :src="site.imagem" :alt="site.nome"/>
-            </div>
-            <h3 class="mb-5 nomesite text-center">{{ site.nome.toUpperCase() }}</h3>
-            <div class="texto">
-                
-                <p class="text-justify  ml-2 mr-2" >{{ site.descricao }}</p>
-            </div>
-            <v-divider class = "mr-4 ml-4"></v-divider>
+          <div>
+            <img
+              class="center home-image mt-4 mb-4 mr-4 ml-4"
+              :src="site.imagem"
+              :alt="site.nome"
+            />
+          </div>
+          <a :href="site.site">
+            <h3 class="mb-5 nomesite text-center">
+              {{ site.nome.toUpperCase() }}
+            </h3>
+          </a>
+          <div class="texto">
+            <p class="text-justify ml-2 mr-2">{{ site.descricao }}</p>
+          </div>
+          <v-divider class="mr-4 ml-4"></v-divider>
         </tr>
       </tbody>
     </template>
@@ -20,32 +27,30 @@
 
 <script>
 export default {
-  name: 'SitesTabela',
+  name: "SitesTabela",
   props: {
     sitesLista: {
       type: Array,
-      required: true
-    }
-  }
-}
+      required: true,
+    },
+  },
+};
 </script>
 
 <style scoped>
 .linha-tabela img {
   width: 200px;
-    
 }
 div {
-    text-align: center;
+  text-align: center;
 }
-.nomesite{
-    font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-    color: purple;
-    font-weight: 600;
-}
-
-.texto{
-    background-color: WhiteSmoke;
+.nomesite {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  color: purple;
+  font-weight: 600;
 }
 
+.texto {
+  background-color: WhiteSmoke;
+}
 </style>
